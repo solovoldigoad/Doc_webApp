d013f1c877b91e8125c48460374021ba3dacfb3e
Kush Chauhan <kushchauhan2004@gmail.com>
2024-06-19 00:01:23 +0530

    vercel in frontend error 3

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   build: {
     outDir: 'dist', // Ensure this matches the directory specified in vercel.json
     emptyOutDir: true, // Clean output directory before each build
@@ -13,4 +11,5 @@ export default defineConfig({
       },
     },
   },
-})
+});
+
