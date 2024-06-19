b9ff4ef89ada687e063a5b81438cfa3b98b0a458
Kush Chauhan <kushchauhan2004@gmail.com>
2024-06-20 01:33:14 +0530

    commmit 4

--- Frontend/vite/vite.config.js
+++ Frontend/vite/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // build: {
-  //   outDir: 'dist', // Ensure this is set to 'dist' or your desired output directory
-  // }
+  build: {
+    outDir: 'dist', // Ensure this is set to 'dist' or your desired output directory
+  }
 })
